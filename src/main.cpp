34e285e09d240a74e39e303aa932e248b7258ff9
skearya <77034153+skearya@users.noreply.github.com>
2024-12-31 00:47:07 -0500

    pixels

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "raylib-cpp.hpp"
+#include <print>
 
 int main() {
-    raylib::Window window(800, 450, "raylib-cpp [core] example - basic window");
+    constexpr int window_width = 800;
+    constexpr int window_height = 400;
+
+    raylib::Window window(window_width, window_height, "raylib-cpp [core] example - basic window");
 
     window.SetTargetFPS(60);
 
@@ -9,7 +13,24 @@ int main() {
         window.BeginDrawing();
 
         window.ClearBackground(raylib::Color::RayWhite());
-        raylib::DrawText("Congrats! You created your first window!", 190, 200, 20, raylib::Color::Black());
+        raylib::DrawText("Congrats! You created your first window!", 800, 400, 20, raylib::Color::Black());
+
+        constexpr int rows = 32;
+        constexpr int cols = 64;
+
+        bool pixels[rows][cols] = {};
+
+        constexpr float pixel_width = static_cast<float>(window_width) / static_cast<float>(cols);
+        constexpr float pixel_height = static_cast<float>(window_height) / static_cast<float>(rows);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                raylib::Rectangle(static_cast<float>(j) * pixel_width,
+                                  static_cast<float>(i) * pixel_height,
+                                  pixel_width, pixel_height).Draw(
+                    pixels[i][j] ? raylib::Color::White() : raylib::Color::Black());
+            }
+        }
 
         window.EndDrawing();
     }
