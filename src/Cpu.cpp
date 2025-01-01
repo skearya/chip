5f4b944d014e28046a6bfbfb828d5d5681777d83
skearya <x@example.com>
2025-01-01 00:49:47 -0500

    stuff

---
+++ src/Cpu.cpp
@@ -0,0 +1,128 @@
+#include "Cpu.hpp"
+#include <cstdint>
+
+Cpu::Cpu(std::span<uint16_t> opcodes) {
+};
+
+void Cpu::tick(const uint16_t opcode) {
+    // ReSharper disable once CppDefaultCaseNotHandledInSwitchStatement (All cases handled)
+    switch (opcode & 0xF000) {
+        case 0x0000: {
+            switch (opcode) {
+                case 0x00E0: {
+                    for (auto row: pixels) {
+                        row.fill(false);
+                    }
+                    break;
+                }
+                case 0x00EE: {
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            break;
+        }
+        case 0x1000: {
+            break;
+        }
+        case 0x2000: {
+            break;
+        }
+        case 0x3000: {
+            bool skip = registers[opcode & 0x0100 >> 8] == (opcode & 0x0011);
+            break;
+        }
+        case 0x4000: {
+            bool no_skip = registers[opcode & 0x0100 >> 8] != (opcode & 0x0011);
+            break;
+        }
+        case 0x5000: {
+            bool skip = registers[opcode & 0x0100 >> 8] != (opcode & 0x0010 >> 4);
+            break;
+        }
+        case 0x6000:
+            registers[opcode & 0x0100 >> 8] = static_cast<char>(opcode & 0x0011);
+            break;
+        case 0x7000: {
+            // char + char = int
+            registers[opcode & 0x0100 >> 8] = static_cast<char>(
+                registers[opcode & 0x0100 >> 8] + static_cast<char>(opcode & 0x0011));
+
+            break;
+        }
+        case 0x8000: {
+            switch (opcode & 0x800F) {
+                case 0x8000: {
+                    break;
+                }
+                case 0x8001: {
+                    break;
+                }
+                case 0x8002: {
+                    break;
+                }
+                case 0x8003: {
+                    break;
+                }
+                case 0x8004: {
+                    break;
+                }
+                case 0x8005: {
+                    break;
+                }
+                case 0x8006: {
+                    break;
+                }
+                case 0x8007: {
+                    break;
+                }
+                case 0x800E: {
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            break;
+        }
+        case 0x9000: {
+            break;
+        }
+        case 0xA000: {
+            break;
+        }
+        case 0xB000: {
+            break;
+        }
+        case 0xC000: {
+            break;
+        }
+        case 0xD000: {
+            break;
+        }
+        case 0xE000: {
+            break;
+        }
+        case 0xF000: {
+            break;
+        }
+    }
+}
+
+void Cpu::push(const uint16_t address) {
+    if (stack_pointer == stack.size()) {
+        throw std::runtime_error("Stack overflowed");
+    }
+
+    stack[stack_pointer++] = address;
+}
+
+uint16_t Cpu::pop() {
+    if (stack_pointer == 0) {
+        throw std::runtime_error("Tried pop with 0 elements");
+    }
+
+    return stack[--stack_pointer];
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "raylib-cpp.hpp"
 #include <print>
+#include <functional>
+#include <iostream>
+
+#include "Cpu.hpp"
 
 int main() {
+    // Cpu cpu;
+
     constexpr int window_width = 800;
     constexpr int window_height = 400;
 
@@ -12,23 +18,25 @@ int main() {
     while (!window.ShouldClose()) {
         window.BeginDrawing();
 
-        window.ClearBackground(raylib::Color::RayWhite());
+        window.ClearBackground(raylib::Color::Black());
         raylib::DrawText("Congrats! You created your first window!", 800, 400, 20, raylib::Color::Black());
 
         constexpr int rows = 32;
         constexpr int cols = 64;
 
-        bool pixels[rows][cols] = {};
+        constexpr bool pixels[rows][cols] = {true};
 
         constexpr float pixel_width = static_cast<float>(window_width) / static_cast<float>(cols);
         constexpr float pixel_height = static_cast<float>(window_height) / static_cast<float>(rows);
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                raylib::Rectangle(static_cast<float>(j) * pixel_width,
-                                  static_cast<float>(i) * pixel_height,
-                                  pixel_width, pixel_height).Draw(
-                    pixels[i][j] ? raylib::Color::White() : raylib::Color::Black());
+                if (pixels[i][j]) {
+                    raylib::Rectangle(static_cast<float>(j) * pixel_width,
+                                      static_cast<float>(i) * pixel_height,
+                                      pixel_width, pixel_height).Draw(
+                        pixels[i][j] ? raylib::Color::White() : raylib::Color::Black());
+                }
             }
         }
 
